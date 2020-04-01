4cc18559e942ac8320c33f6b5594bbc9f9a91920
wrmsr <timwilloney@gmail.com>
2020-03-31 21:53:06 -0700

    --

--- rsws/pyadder/src/lib.rs
+++ rsws/pyadder/src/lib.rs
@@ -3,10 +3,12 @@ extern crate pyo3;
 
 use pyo3::prelude::*;
 
+use add_one;
+
 
 #[pyfunction]
-fn ret_one(line: &str, needle: &str) -> usize {
-    1
+fn ret_one() -> i32 {
+    add_one::add_one(0)
 }
 
 #[pymodule]
