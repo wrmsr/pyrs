7b636eb5a65042c924fd5e891b4b046f42e57244
wrmsr <timwilloney@gmail.com>
2020-04-01 09:27:24 -0700

    --

--- rsws/pyadder/src/lib.rs
+++ rsws/pyadder/src/lib.rs
@@ -3,7 +3,7 @@ extern crate pyo3;
 
 use pyo3::prelude::*;
 
-use add_one;
+use pydicts::{FromPyObject, IntoPyObject};
 
 
 #[pyfunction]
@@ -11,9 +11,32 @@ fn ret_one() -> i32 {
     add_one::add_one(0)
 }
 
+#[derive(FromPyObject, IntoPyObject)]
+struct User {
+    name: String,
+    email: String,
+    age: u32,
+}
+
+#[pyfunction]
+fn get_contact_info(user: User) -> PyResult<String> {
+    Ok(format!("{} - {}", user.name, user.email))
+}
+
+#[pyfunction]
+fn get_default_user() -> PyResult<User> {
+    Ok(User {
+        name: "Default".to_owned(),
+        email: "default@user.com".to_owned(),
+        age: 27,
+    })
+}
+
 #[pymodule]
 fn pyadder(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(ret_one))?;
+    m.add_wrapped(wrap_pyfunction!(get_contact_info))?;
+    m.add_wrapped(wrap_pyfunction!(get_default_user))?;
 
     Ok(())
 }
