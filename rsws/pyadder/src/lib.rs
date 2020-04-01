7090be164a9a6ef42c6f1238e49a0db44c5e181b
wrmsr <timwilloney@gmail.com>
2020-03-31 21:28:53 -0700

    --

--- rsws/pyadder/src/lib.rs
+++ rsws/pyadder/src/lib.rs
@@ -1,3 +1,21 @@
+#[macro_use]
+extern crate pyo3;
+
+use pyo3::prelude::*;
+
+
+#[pyfunction]
+fn ret_one(line: &str, needle: &str) -> usize {
+    1
+}
+
+#[pymodule]
+fn word_count(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(ret_one))?;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
